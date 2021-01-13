fc812e4e4a25ac34311f1c849a03cdabe3aa34b8
felix.niederwanger@suse.com <phoenix>
2021-01-13 10:43:06 +0100

    Add h5py test to scientifc-python tests
    Adds a test for h5py to the scientific-python test case

---
+++ data/python/python3-h5py-test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Test h5py functions
+# Maintainer: Felix Niederwanger <felix.niederwanger@suse.de>
+
+
+import math
+
+import numpy as np
+import h5py
+
+
+if __name__ == "__main__":
+	# TODO: Write test
+    print("OK")
